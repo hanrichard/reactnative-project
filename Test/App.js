206e520a1e9d249b14d8cb4859834c36ddff61ab
richardhan <hxy.314@gmail.com>
2021-06-10 23:26:03 +1000

    add todo

--- Test/App.js
+++ Test/App.js
@@ -1,96 +1,78 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {useState} from 'react';
 import {
+  Button,
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  TextInput,
+  FlatList,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const initialState = [
+    {id: 1, title: 'test 1'},
+    {id: 2, title: 'test 2'},
+    {id: 3, title: 'test 2'},
+  ];
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const [text, setText] = useState('');
+  const [todo, setTodo] = useState(initialState);
+  const onPressLearnMore = () => {
+    setTodo([...todo, {id: todo.length + 1, title: text}]);
+    setText('');
   };
 
+  const onChangeText = value => {
+    setText(value);
+  };
+
+  const Item = ({title}) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+
+  const renderItem = ({item}) => <Item title={item.title} />;
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.containerStyle}>
+        <Text>Todo list</Text>
+        <View style={styles.containerRow}>
+          <FlatList
+            data={todo}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+          />
         </View>
-      </ScrollView>
+        <View style={styles.containerRow}>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            value={text}
+          />
+        </View>
+        <View style={styles.containerRow}>
+          <Text>
+            <Button title="Add" onPress={onPressLearnMore} />
+          </Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  containerStyle: {
+    height: '100%',
+    alignItems: 'center',
+  },
+  containerRow: {
+    flexDirection: 'row',
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
@@ -106,6 +88,20 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'black',
+    width: '100%',
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
 
