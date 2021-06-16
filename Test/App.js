d60ec4770209b6d56b0a8923e87c55de067c273b
richardhan <hxy.314@gmail.com>
2021-06-16 22:44:21 +1000

    test

--- Test/App.js
+++ Test/App.js
@@ -23,10 +23,6 @@ function DetailsScreen({navigation, route}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.navigate('Details')}
-      />
       <Button title="Go back" onPress={() => navigation.goBack()} />
       <Button
         title="Go back to first screen in stack"
